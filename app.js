2f7451796f726525d57e7231920fae891c244e6b
Anand raja <46064269+actionanand@users.noreply.github.com>
2022-01-10 16:16:58 +0530

    attack works

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('Hi');
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      playersHealth: 100,
+      monstersHealth: 100
+    };
+  },
+  computed: {
+    playersHealthBar() {
+      return {width: this.playersHealth + '%'};
+    },
+    monstersHealthBar() {
+      return {width: this.monstersHealth + '%'};
+    }
+  },
+  methods: {
+    attackMonster() {
+      const attackValue = getRandomValue(5, 12);
+      this.monstersHealth -= attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = getRandomValue(8, 15);
+      this.playersHealth -= attackValue;
+    }
+  }
+});
+
+app.mount('#game');
